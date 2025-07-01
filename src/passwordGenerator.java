a37d826c346581835b94e4e3b3428de2f7419c77
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-07-01 20:32:46 +0200

    Password generator

---
+++ src/passwordGenerator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class passwordGenerator {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Inserisci il tuo nome: ");
+        String name = scan.next();
+
+        System.out.print("Inserisci il tuo cognome: ");
+        String surname = scan.next();
+
+        System.out.print("Inserisci il tuo colore preferito: ");
+        String favouriteColor = scan.next();
+
+        System.out.print("Inserisci la tua data di nascita (DD/MM/YYYY): ");
+        String dateOfBirth = scan.next();
+
+        String dayString = dateOfBirth.substring(0, 2);
+        String monthString = dateOfBirth.substring(3, 5);
+        String yearString = dateOfBirth.substring(6, 10);
+
+        int day = Integer.parseInt(dayString);
+        int month = Integer.parseInt(monthString);
+        int year = Integer.parseInt(yearString);
+
+        int dateSum = day + month + year;
+
+        System.out.print("la tua password è " + name + "-" + surname + "-" + favouriteColor + "-" + dateSum);
+
+    }
+}
+
+
